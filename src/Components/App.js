916c8a663ce42694f04c8ff6110a75de8e9c3412
renaatosalgado <renaato.salgado@gmail.com>
2021-12-04 15:32:48 -0300

    final version

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import Topbar from "./Topbar";
+import Main from "./Main";
+import BottomMobile from "./BottomMobile";
+
+export default function App () {
+    return (
+        <div>
+            <Topbar />
+            <Main />
+            <BottomMobile />
+        </div>
+    );
+}
